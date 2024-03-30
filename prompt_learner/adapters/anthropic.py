8acce3a566219e0c3355a550ade05d8125c35b1b
Aditya Lahiri <adityalahiri13@gmail.com>
2024-03-29 22:11:36 -0700

    style adapters

--- prompt_learner/adapters/anthropic.py
+++ prompt_learner/adapters/anthropic.py
@@ -1,8 +1,17 @@
+"""This module contains the Anthropic class,
+which is an adapter for the Anthropic language model API."""
+
+import os
 from langchain_anthropic import ChatAnthropic
 from dotenv import load_dotenv
-import os
+
 
 class Anthropic:
+    """An adapter for an Anthropic language model call"""
     def __init__(self):
         load_dotenv()
-        self.llm = ChatAnthropic(anthropic_api_key=os.getenv('ANTHROPIC_API_KEY'),model="claude-3-haiku-20240307", temperature=0.0, max_tokens=1024)
+        self.llm = ChatAnthropic(
+                anthropic_api_key=os.getenv('ANTHROPIC_API_KEY'),
+                model="claude-3-haiku-20240307",
+                temperature=0.0,
+                max_tokens=1024)
--- prompt_learner/adapters/openai.py
+++ prompt_learner/adapters/openai.py
@@ -1,8 +1,16 @@
+"""This module contains the OpenAI class
+which is used to interact with the OpenAI API."""
+
+import os
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-import os
+
 
 class OpenAI:
+    """An adapter for an OpenAI language model call"""
     def __init__(self):
         load_dotenv()
-        self.llm = ChatOpenAI(openai_api_key = os.getenv('OPENAI_API_KEY'),temperature=0.0, max_tokens=1024)
+        self.llm = ChatOpenAI(
+            openai_api_key=os.getenv('OPENAI_API_KEY'),
+            temperature=0.0,
+            max_tokens=1024)
